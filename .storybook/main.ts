0cb2ed3ecf8ea958b25517e5a372ea48fb0fa51b
Alek Racz <raczaig@gmail.com>
2025-03-07 09:50:19 -0600

    fix ts issues

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -14,6 +14,9 @@ const config: StorybookConfig = {
   "framework": {
     "name": "@storybook/react-vite",
     "options": {}
+  },
+  "core": {
+    "builder": "storybook-builder-vite",
   }
 };
 export default config;
