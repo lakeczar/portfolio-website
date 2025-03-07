027fb1f457e3c92cc3100333b734f93231406f39
Alek Racz <raczaig@gmail.com>
2025-03-07 09:16:01 -0600

    SETUP

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,24 @@ import tailwindcss from '@tailwindcss/vite'
 
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    strictPort: true,
-    cors: true
+export default defineConfig(({ mode }) => {
+  
+  const isGitHubPages = mode === 'github'
+  
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    base: isGitHubPages ? '/portfolio-website/' : '/',
+    build: {
+      outDir: 'dist',
+    },
+    server: {
+      host: '0.0.0.0',
+      port: 5173,
+      strictPort: true,
+      cors: true
+    }
   }
 })
