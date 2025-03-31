b2f198a83ae190f0e39934a23d19d607bec479a1
Alek Racz <raczaig@gmail.com>
2025-03-31 13:23:00 -0500

    back to normal

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-import { motion } from "framer-motion"
+import { motion } from "motion/react"
 import BlurredLight from "../blur-lights/BlurLights"
 import alekRaczProfile from '../../assets/images/alekRaczProfile.jpg'
 import { blurLights } from "../../utils/blurLightsConstants"
